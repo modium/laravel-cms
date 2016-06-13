1b8e60f306f653fefd25b0fd6a2696fbdf8e29c5
Jaf Crisologo <jafcrisologo@Jafs-MacBook-Pro.local>
2016-06-12 23:19:24 -0600

    added Create User form, custom request and Photo model/migration

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use App\Role;
+use App\Http\Requests\UsersRequest;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -17,7 +20,9 @@ class AdminUsersController extends Controller
     {
         //
 
-        return view('admin.users.index');
+        $users = User::all();
+
+        return view('admin.users.index', compact('users'));
 
     }
 
@@ -30,7 +35,9 @@ class AdminUsersController extends Controller
     {
         //
 
-        return view('admin.users.create');
+        $roles = Role::lists('name','id')->all(); // retrieve all available roles
+
+        return view('admin.users.create', compact('roles'));
 
     }
 
@@ -40,9 +47,16 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
         //
+
+        // return $request->all();
+
+        User::create($request->all());
+
+        return redirect('/admin/users');
+
     }
 
     /**
